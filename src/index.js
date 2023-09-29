ba449608c23219a6d928456a2bec6c32d21613f6
MeF1sT1k <just.subway@gmail.com>
2023-09-29 22:54:56 +0300

    training babel & webpack

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/app';
 
+
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
